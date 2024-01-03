a3323b643b3560dd9e480f58275d83278f5254c8
Grzegorz Kraszewski <grzegorz.kraszewski@gmail.com>
2024-01-03 18:23:09 +0100

    Removed not used definitions.

--- src/main.h
+++ src/main.h
@@ -1,36 +1,12 @@
-/* Structure holding all the GUI stuff. */
-
-
 extern struct Library
 	*SysBase,
 	*DOSBase,
 	*UtilityBase,
 	*MathIeeeSingBasBase,
-	*MathIeeeSingTransBase;
-
-struct Rect
-{
-	WORD x;
-	WORD y;
-	WORD w;
-	WORD h;
-};
-
-
-struct Gui
-{
-	struct Screen    *g_Screen;
-	struct Window    *g_Window;
-	struct DrawInfo  *g_DrawInfo;
-	WORD              g_TextSize;
-	WORD              g_BorderThk;
-	WORD              g_WinWidth;
-	WORD              g_WinHeight;
-	struct Rect       g_SampleView;
-};
-
+	*MathIeeeSingTransBase,
+	*IFFParseBase;
+	
 extern ULONG MorErr;
-
 
 static inline SetErr(LONG retval, LONG ecode)
 {
